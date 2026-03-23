0897d9c1cba33f8850fe7c17f528791547e49fa4
vbells <vbelloh@gmail.com>
2026-03-22 23:15:55 -0500

    Show camera feed in QR scanner
    Added a line to display the camera feed while scanning QR codes.

--- qr_decoder.py
+++ qr_decoder.py
@@ -37,6 +37,7 @@ while True:
             print("Error: QR code data is not valid JSON\n")
 
     # q to quit
+    cv2.imshow("QR Scanner", frame) # to view camera feed 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
