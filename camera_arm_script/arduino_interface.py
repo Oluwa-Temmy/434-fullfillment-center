6b4d22d92afd9d215e848a96393bd400fd563ba9
code-made-a-line <madelinemendoza317@gmail.com>
2026-04-26 17:34:36 -0500

    fixed spelling in script comment

--- camera_arm_script/arduino_interface.py
+++ camera_arm_script/arduino_interface.py
@@ -10,7 +10,7 @@ from time import sleep
     Arduino, send and receive data, and manage the serial connection. """
 
 class ArduinoInterface:
-    """ this class ahngles the communication with the arduino. 
+    """ this class allows for communication with the arduino. 
     It detects the port, sends commands to arduino and establishes and closes serial connection.
    """
     
